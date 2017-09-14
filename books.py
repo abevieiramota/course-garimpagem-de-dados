f3e877709e19818459a7473ff891d766656f1928
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-09-14 11:01:42 -0300

    move p script código de leitura de livros; adiciona knn

---
+++ books.py
@@ -0,0 +1,40 @@
+import codecs
+import re
+import nltk
+
+re_empty_line = re.compile(r'^\s*$')
+re_remove_chars = re.compile("[\r\n•]+")
+re_chapter_title = re.compile("^[A-ZÁÉÍÓÚÃÕ\. ]+\s*$")
+
+def clean_line(line):
+    
+    return re_remove_chars.sub("", line)
+
+def is_good_line_casmurro(line):
+    
+    return re_empty_line.match(line) is None and\
+           re_chapter_title.match(line) is None
+    
+def is_good_line_clarice(line):
+    
+    return re_empty_line.match(line) is None
+    
+def clean_and_filter_casmurro(lines):
+    
+    return [line for line in [clean_line(line) for line in lines] if \
+            is_good_line_casmurro(line)]
+
+def clean_and_filter_clarice(lines):
+    
+    return [line for line in [clean_line(line) for line in lines] if \
+            is_good_line_clarice(line)]
+
+sent_tokenizer = nltk.data.load("tokenizers/punkt/portuguese.pickle")
+
+with codecs.open('dom_casmurro.txt', 'r', 'utf-8') as f:
+    
+    dom_casmurro = sent_tokenizer.tokenize(' '.join(clean_and_filter_casmurro(f.readlines())))
+    
+with codecs.open('perto_do_coracao_selvagem.txt', 'r', 'utf-8') as f:
+    
+    perto_coracao = sent_tokenizer.tokenize(' '.join(clean_and_filter_clarice(f.readlines())))
